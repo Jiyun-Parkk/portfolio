3a8a940934448202b3dee2312cef53de818862df
박지윤 <jyun1937@naver.com>
2023-01-03 21:38:30 +0900

    Feat: GA 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,50 @@ import { dark, light } from 'styles/theme'
 import GlobalStyle from 'styles/global.styled'
 import { ThemeProvider } from 'styled-components'
 import wrapper from 'store'
+import { useEffect } from 'react'
+import * as gtag from 'lib/gtag'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
 
 function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
   const isDark = useAppSelector((state) => state.theme.value)
   const isModalOpen = useAppSelector((state) => state.modal.value)
 
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [])
   return (
     <>
+      {/* GA 설정 시작 */}
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${gtag.GA_TRACKING_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+        }}
+      />
+      {/* GA 설정 끝 */}
       <ThemeProvider theme={isDark ? dark : light}>
         <GlobalStyle isOpen={isModalOpen} />
         <Layout>
